cd297e7144a2b085c2cf481712d8196854c2a462
qkdtjwns123-del <qkdtjwns123@gmail.com>
2025-11-06 22:12:16 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js  (HTML 태그 넣지 마세요!)
+window.firebaseConfig = {
+    apiKey: "AIzaSyB0TUXQpzZIy0v2gbLOC343Jx_Lv51EQvw",
+    authDomain: "honbap-paring.firebaseapp.com",
+    projectId: "honbap-paring",
+    storageBucket: "honbap-paring.firebasestorage.app",
+    messagingSenderId: "375771626039",
+    appId: "1:375771626039:web:03868631de56225cf49db2",
+};
+
+// ✅ 관리자 이메일 목록(원하면 여러 개 추가 가능)
+window.ADMIN_EMAILS = ["qkdtjwns123@kw.ac.kr"];
